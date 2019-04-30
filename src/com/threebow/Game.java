cc5fe385bab9c3e35341d4bae18bf079da25a83f
Alex <threebowpr@gmail.com>
2019-04-29 17:54:53 -0700

    Make game board local

--- src/com/threebow/Game.java
+++ src/com/threebow/Game.java
@@ -1,10 +1,8 @@
 package com.threebow;
 
 class Game {
-	private Board board;
-
 	Game() {
-		board = new Board(8, 8);
+		Board board = new Board(8, 8);
 		board.generate(10);
 		board.printGrid();
 	}
